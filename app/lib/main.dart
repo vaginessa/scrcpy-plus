73daaaa6c52b78a66cf14e10da5e78bf20c00fa8
Kenny <27463495+Frontesque@users.noreply.github.com>
2023-12-01 23:39:42 -0500

    basic layout

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -90,27 +90,27 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center, // Center y-axis
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Image.asset('assets/logo.png', height: 100),
             Text(
-              '$_counter',
+              'SCRCPY+',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text('$_counter'),
+            Card(
+              elevation: 0,
+              color: Theme.of(context).colorScheme.surfaceVariant,
+              child: const SizedBox(
+                width: 300,
+                height: 100,
+                child: Column(children: [
+                  Text("Model"),
+                  Text("Android Version"),
+                  Text("System Build Number"),
+                  Text("Security Patch"),
+                ]),
+              ),
             ),
           ],
         ),
